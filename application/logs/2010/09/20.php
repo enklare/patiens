b0dc38e0aff9bc860bf106813bc4a9f746a537f5
Pontus Kindblad <enklare.kb@gmail.com>
2010-09-23 01:08:22 +0200

    laddar upp allt

--- application/logs/2010/09/20.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-09-20 23:34:34 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 638 ]
-2010-09-20 23:34:42 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 638 ]
-2010-09-20 23:34:44 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 638 ]
-2010-09-20 23:34:46 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 638 ]
-2010-09-20 23:59:54 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: favicon.ico ~ SYSPATH/classes/kohana/request.php [ 638 ]
